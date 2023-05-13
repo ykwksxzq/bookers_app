2b1bdbef8ce2e6facf4cade0c9a98e6cc0930408
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-05-13 09:13:29 +0000

    [add]ファイルの作成準備

---
+++ db/migrate/20230513080810_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title #本のタイトル
+      t.string :body #感想
+
+      t.timestamps
+    end
+  end
+end

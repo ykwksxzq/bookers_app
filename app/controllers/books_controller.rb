559ce34a43d98bf6f0efb351de2d169f4e6cb45b
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-05-14 12:52:52 +0000

    [add]詳細機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new (book_params)
     @book.save
-    redirect_to'/top'
+    redirect_to book_path(@book.id)
   end
 
   def index

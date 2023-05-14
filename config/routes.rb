6ca4bdae7ef0e4e7d06721cd1aaf50223ed4c463
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-05-14 06:57:02 +0000

    [add]投稿機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get '/top' => 'homes#top'
   get 'books/new'
-  get 'books/index'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
   get 'books/show'
   get 'books/edit'
-  get '/top' => 'homes#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

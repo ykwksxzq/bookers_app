6be6af772ba137a0118c9d4e55ce21b48ba0cf21
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-05-15 13:15:28 +0000

    [add]編集機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as:'book'
-  get 'books/edit'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'lists/:id' => 'books#destroy', as: 'destory_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

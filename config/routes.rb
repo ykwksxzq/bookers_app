f9b093ad1a1d4fb96e79ef1a3d9ec95ae3545139
ykwksxzq <132530452+ykwksxzq@users.noreply.github.com>
2023-05-18 15:50:59 +0000

    [add]メッセージの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
  
   get 'top' => 'homes#top'
-  # post 'books' => 'books#create'
-  # get 'books' => 'books#index'
-  # get 'books/:id' => 'books#show', as: 'book'
-  # get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  # patch 'books/:id' => 'books#update', as: 'update_book'
-  # delete 'lists/:id' => 'books#destroy', as: 'destroy_book'
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
